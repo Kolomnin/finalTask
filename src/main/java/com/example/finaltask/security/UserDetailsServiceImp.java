f845cbaacf99f7274c75ca6c469df78f5a4ae871
northin <northin@gmail.com>
2023-07-03 20:20:36 +0300

    serviceuser

--- src/main/java/com/example/finaltask/security/UserDetailsServiceImp.java
+++ src/main/java/com/example/finaltask/security/UserDetailsServiceImp.java
@@ -1,13 +1,25 @@
 package com.example.finaltask.security;
 
+import com.example.finaltask.model.entity.User;
+import com.example.finaltask.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImp(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(username).orElseThrow();
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRole()))
+                .build();
     }
 }
