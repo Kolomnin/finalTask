c2c0748e1af83bb17031b1a4bb3880f76bc6a94a
northin <northin@gmail.com>
2023-07-04 07:47:50 +0300

    serviceuser

--- src/main/java/com/example/finaltask/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/finaltask/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.finaltask.configuration;
 
+import com.example.finaltask.security.UserDetailsServiceImp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -29,15 +31,8 @@ public class WebSecurityConfig {
     };
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user =
-                User.builder()
-                        .username("user@gmail.com")
-                        .password("password")
-                        .passwordEncoder((plainText) -> passwordEncoder().encode(plainText))
-                        .roles("USER")
-                        .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImp();
     }
 
     @Bean
